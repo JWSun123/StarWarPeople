743069dc91ffcdacd99c8b8b038b2821ab9f627c
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-02-20 16:52:38 -0500

    putmapping, postmapping, deletemapping

--- complete/src/main/java/com/example/restservice/api/PeopleController.java
+++ complete/src/main/java/com/example/restservice/api/PeopleController.java
@@ -5,9 +5,9 @@ import com.example.restservice.model.People;
 import com.example.restservice.service.PeopleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class PeopleController {
@@ -23,5 +23,35 @@ public class PeopleController {
             return new ResponseEntity(peopleNotFoundException.getMessage(),HttpStatus.NOT_FOUND);
         }
         //return new ResponseEntity<>(peopleService.getPeople(id),HttpStatus.OK);
+    }
+
+    @GetMapping("/getAllPeople")
+        public ResponseEntity<ArrayList<People>> getAllPeople(){
+            return new ResponseEntity(peopleService.getAllPeople(), HttpStatus.OK);
+        }
+
+    @PostMapping("/people")
+    public ResponseEntity<People> postPeople(@RequestBody People people){
+        if(peopleService.addPeople(people) == true){
+            return new ResponseEntity<>(people, HttpStatus.OK);
+        }
+        return new ResponseEntity("Could not add", HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping("/updatePerson/{name}")
+    public ResponseEntity<People> updatePerson(@PathVariable String name, @RequestBody People people){
+        People updated = peopleService.updatePerson(name, people);
+        if (updated != null){
+            return new ResponseEntity<People>(updated, HttpStatus.OK);
+        }
+        return new ResponseEntity("Could not find person", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/deletePerson/{name}")
+    public ResponseEntity deletePerson(@PathVariable String name){
+        if(peopleService.deletePerson(name)){
+            return new ResponseEntity("Person " + name + " deleted", HttpStatus.OK);
+        }
+        return new ResponseEntity("Could not delete " + name, HttpStatus.BAD_REQUEST);
     }
 }
--- complete/src/main/java/com/example/restservice/service/PeopleService.java
+++ complete/src/main/java/com/example/restservice/service/PeopleService.java
@@ -4,13 +4,51 @@ import com.example.restservice.adapter.PeopleAdapter;
 import com.example.restservice.exception.PeopleNotFoundException;
 import com.example.restservice.model.People;
 
+import java.util.ArrayList;
+
 
 public class PeopleService{
+
+    static ArrayList<People> peopleList = new ArrayList<>();
+
     public People getPeople(int id) {
         PeopleAdapter peopleAdapter = new PeopleAdapter();
         if(id < 0){
             throw new PeopleNotFoundException("Invalid id");
         }
-        return peopleAdapter.callPeopleApi(id);
+        People people = peopleAdapter.callPeopleApi(id);
+
+        peopleList.add(people);
+        return people;
+    }
+
+    public boolean addPeople(People people){
+        peopleList.add(people);
+        return true;
+    }
+    public ArrayList<People> getAllPeople(){
+        return peopleList;
+    }
+
+    public People updatePerson(String name, People update){
+        for (People people: peopleList){
+            if (people.getName().equals(name)){
+                people.setHeight(update.getHeight());
+                people.setFilms(update.getFilms());
+                people.setHairColor(update.getHairColor());
+                return people;
+            }
+        }
+        return null;
+    }
+
+    public boolean deletePerson(String name){
+        for (People people: peopleList){
+            if (people.getName().equals(name)){
+                peopleList.remove(people);
+                return true;
+            }
+        }
+        return false;
     }
 }
